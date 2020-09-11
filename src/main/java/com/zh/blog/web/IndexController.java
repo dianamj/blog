60d7a9944a9685825177d2e7dd483e1383b7e75f
mujiujiu <zhanghongsk@qq.com>
2020-09-11 08:21:28 +0800

    init

--- src/main/java/com/zh/blog/web/IndexController.java
+++ src/main/java/com/zh/blog/web/IndexController.java
@@ -53,5 +53,11 @@ public class IndexController {
         return "blog";
     }
 
+    @GetMapping("/footer/newblog")
+    public String newblogs(Model model) {
+        model.addAttribute("newblogs", blogService.listRecommendBlogTop(3));
+        return "_fragments :: newblogList";
+    }
+
 
 }
